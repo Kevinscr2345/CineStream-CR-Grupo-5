ec02533ae5500186ee41e5b315059897f2a02d1f
Nachocn1707 <icascante70274@ufide.ac.cr>
2026-07-06 18:46:19 -0600

    feat: correción de calificaciones

--- CineStreamCR/Controllers/ReviewController.cs
+++ CineStreamCR/Controllers/ReviewController.cs
@@ -1,16 +1,20 @@
 using CineStreamCR.BLL.Dtos;
+using CineStreamCR.BLL.Dtos.Review;
 using CineStreamCR.BLL.Services;
+using CineStreamCR.BLL.Services.Review;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CineStreamCR.Controllers;
 
 [Authorize]
+[IgnoreAntiforgeryToken]
 [Route("api/movies")]
 public sealed class ReviewController(IReviewService reviewService) : ApiControllerBase
 {
     [HttpPut("{movieId:int}/review")]
-    public async Task<IActionResult> Save(int movieId, [FromBody] ReviewRequest request, CancellationToken ct) =>
-        FromResponse(await reviewService.UpsertAsync(GetUserId(), movieId, request, ct));
+    public async Task<IActionResult> Save(int movieId, [FromBody] ReviewRequest request, CancellationToken ct)
+    {
+        return FromResponse(await reviewService.UpsertAsync(GetUserId(), movieId, request, ct));
+    }
 }
-
